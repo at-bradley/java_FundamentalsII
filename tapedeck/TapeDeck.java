a9af91f86b03215d10d41fa217e49d8de4030747
Anthony Thudium <thudius.anthony@gmail.com>
2017-10-12 20:38:40 -0700

    Add files via upload

---
+++ tapedeck/TapeDeck.java
@@ -0,0 +1,15 @@
+//p.42
+
+package tapedeck;
+
+class TapeDeck {
+
+    boolean canRecord = false;
+    
+    void playTape() {
+        System.out.println("tape playing");
+    }
+    void recordTape() {
+        System.out.println("tape recording");
+    }
+}
