4e36302bdc2cea8f05aca00ee6c9a8e1abb1c488
Anthony Thudium <thudius.anthony@gmail.com>
2017-10-12 20:39:21 -0700

    Add files via upload

---
+++ dvdplayer/DVDPlayer.java
@@ -0,0 +1,14 @@
+//p.42
+package dvdplayer;
+
+class DVDPlayer {
+    
+    boolean canRecord = false;
+    
+    void recordDVD() {
+        System.out.println("DVD recording");
+    }
+    void playDVD() {
+        System.out.println("DVD playing");
+    }
+    }
---
+++ dvdplayer/DVDPlayerTestDrive.java
@@ -0,0 +1,13 @@
+package dvdplayer;
+
+class DVDPlayerTestDrive {
+    public static void main(String[] args) {
+        DVDPlayer d = new DVDPlayer();
+        d.canRecord = true;
+        d.playDVD();
+        
+        if (d.canRecord == true) {
+            d.recordDVD();
+        }
+    }
+}
