2040c985217a5606229af2e71ae2182a9f2d1431
Anthony Thudium <thudius.anthony@gmail.com>
2017-10-18 19:48:42 -0700

    Add files via upload

---
+++ clock/Clock.java
@@ -0,0 +1,15 @@
+// p.88
+package clock;
+
+class Clock {
+    
+    private String time;
+    
+    public void setTime(String t) {
+        time = t;
+    }
+    
+    public String getTime() {
+        return time;
+    }
+}
---
+++ clock/ClockTestDrive.java
@@ -0,0 +1,15 @@
+
+package clock;
+
+class ClockTestDrive {
+    public static void main(String[] args) {
+        
+        Clock c = new Clock();
+        
+        c.setTime("1245");
+        String tod = c.getTime();
+        
+        System.out.println("time: " + tod);
+    }
+    
+}
